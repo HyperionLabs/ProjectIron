d7932034479bbbe42f44437576560bd533dc4677
Alex Tyler <Alex Tyler>
2016-10-23 17:45:57 -0700

    Small bug fixes and added in health and fuel bar widgets

---
+++ Source/ProjectIron/MP_GameInstance_Beacon.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProjectIron.h"
+#include "MP_GameInstance_Beacon.h"
+
+MP_GameInstance_Beacon::MP_GameInstance_Beacon()
+{
+}
+
+MP_GameInstance_Beacon::~MP_GameInstance_Beacon()
+{
+}
---
+++ Source/ProjectIron/MP_GameInstance_Beacon.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class PROJECTIRON_API MP_GameInstance_Beacon
+{
+public:
+	MP_GameInstance_Beacon();
+	~MP_GameInstance_Beacon();
+};
